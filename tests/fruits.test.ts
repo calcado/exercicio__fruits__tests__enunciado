331aa35fc4ddcb358b7fb63decf938a9bf05a3f2
calcado <pecalcado@gmail.com>
2023-02-01 21:37:24 -0300

    test: get fruit id

--- tests/fruits.test.ts
+++ tests/fruits.test.ts
@@ -25,3 +25,30 @@ describe("GET /fruits", () => {
     ]);
   });
 });
+
+describe("GET /fruits/:id", ()=>{
+
+  it("Should response with status 404 if there is no fruit",async ()=>{
+    const response = await api.get("/fruits/1")    
+    expect(response.status).toEqual(404);
+  })
+
+  it(" Should respond with status 200 and specific fruit", async () =>{
+    const body: FruitInput = {
+      name: "banana",
+      price: 4,
+    };
+
+    await supertest(app).post("/fruits").send(body);
+    const response = await api.get("/fruits/1")
+    
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual(
+      {
+        id: expect.any(Number),
+        name: expect.any(String),
+        price: expect.any(Number),
+      },
+    );
+  });  
+})
